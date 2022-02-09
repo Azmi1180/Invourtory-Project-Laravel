fd89489db6cdb5be688d6f20d1d7c84952fd4f34
Azmi1180 <muhammadazmi4456@gmail.com>
2022-02-09 18:45:48 +0700

    update link untuk tampilan baru

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -152,13 +152,13 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
-                    <span>Admin Controls</span>
+                    <span>Admin </span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Admin Pages</h6>
-                        <a class="collapse-item" href="buttons.html">User Control</a>
-                        <a class="collapse-item" href="cards.html">Item Control</a>
+                        <a class="collapse-item" href="{{ route('admin.users.index') }}">Manage Users</a>
+                        <a class="collapse-item" href="/barang/index">Manage Items</a>
                     </div>
                 </div>
             </li>
@@ -168,16 +168,16 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>Utilities</span>
+                    <span>Simpan Pinjam</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="utilities-color.html">Colors</a>
-                        <a class="collapse-item" href="utilities-border.html">Borders</a>
-                        <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                        <a class="collapse-item" href="utilities-other.html">Other</a>
+                        <h6 class="collapse-header">Global Pages:</h6>
+                        <a class="collapse-item" href="/barang/index">Daftar Barang</a>
+                        <a class="collapse-item" href="/inventory/index">Inventory</a>
+                        {{-- <a class="collapse-item" href="utilities-animation.html">Animations</a>
+                        <a class="collapse-item" href="utilities-other.html">Other</a> --}}
                     </div>
                 </div>
             </li>
@@ -461,17 +461,17 @@
             @yield('content')
         </div>
 
-        <!-- Footer -->
-        <footer class="sticky-footer bg-white">
-            <div class="container my-auto">
-                <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; INVOURTORY 2022</span>
-                </div>
-            </div>
-        </footer>
-        <!-- End of Footer -->
+        
     </div>
-
+    <!-- Footer -->
+    <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+                <span>Copyright &copy; INVOURTORY 2022</span>
+            </div>
+        </div>
+    </footer>
+    <!-- End of Footer -->
     <!-- Scroll to Top Button-->
     <a class="scroll-to-top rounded" href="#page-top">
         <i class="fas fa-angle-up"></i>
